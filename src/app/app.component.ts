870bbafe1f795fbea778dd007a2f7b701d81751f
Thomas Modolo <thomas.modolo@ynov.com>
2022-03-02 12:01:34 +0100

    feat : working with expressjs back end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fish-eye';
+  server = 'http://64f5-2a01-cb19-825b-1c00-d84c-3fa0-4d27-a5d7.ngrok.io/api'
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const response = this.http.get(`${this.server}/test`, {responseType: 'text'}).subscribe(resp => console.log(resp))
+    // console.log(response)
+    // response.subscribe(resp => console.log(resp))
+    console.log('non toi')
+  }
+
 }
